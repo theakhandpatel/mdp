d3d8e8d0b2f2e856801107b571912381441c127f
akhand <akhand3108patel@gmail.com>
2024-05-21 00:49:02 +0530

    INIT:AKHAND: Custom templates

---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"os"
+	"os/exec"
+	"runtime"
+	"time"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/russross/blackfriday/v2"
+)
+
+const (
+	defaultTemplate = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>{{ .Title }}</title>
+  </head>
+  <body>
+{{ .Body }}
+  </body>
+</html>
+`
+)
+
+type content struct {
+	Title string
+	Body  template.HTML
+}
+
+func main() {
+	// Parse flags
+	filename := flag.String("file", "", "Markdown file to preview")
+	skipPreview := flag.Bool("s", false, "Skip auto-preview")
+	tfName := flag.String("t", "", "Alternative template name")
+	flag.Parse()
+
+	// If user did not provide input file, show usage
+	if *filename == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if err := run(*filename, *tfName, os.Stdout, *skipPreview); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(filename string, tfName string, out io.Writer, skipPreview bool) error {
+	input, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	htmlData, err := parseContent(input, tfName)
+	if err != nil {
+		return err
+	}
+	temp, err := os.CreateTemp("", "mdp*.html")
+	if err != nil {
+		return err
+	}
+	if err := temp.Close(); err != nil {
+		return err
+	}
+
+	outName := temp.Name()
+	fmt.Fprintln(out, outName)
+
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+	if skipPreview {
+		return nil
+	}
+	defer os.Remove(outName)
+	return preview(outName)
+}
+
+func parseContent(input []byte, tfName string) ([]byte, error) {
+	output := blackfriday.Run(input)
+	body := bluemonday.UGCPolicy().SanitizeBytes(output)
+
+	t, err := template.New("mdp").Parse(defaultTemplate)
+	if err != nil {
+		return nil, err
+	}
+	if tfName != "" {
+		t, err = template.ParseFiles(tfName)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	c := content{
+		Title: "Markdown Preview Tool",
+		Body:  template.HTML(body),
+	}
+
+	var buffer bytes.Buffer
+	if err := t.Execute(&buffer, c); err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+
+}
+
+func saveHTML(outFname string, data []byte) error {
+	return os.WriteFile(outFname, data, 0644)
+}
+
+func preview(fname string) error {
+	cName := ""
+	cParams := []string{}
+
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "cmd.exe"
+		cParams = []string{"/C", "start"}
+	case "darwin":
+		cName = "open"
+	default:
+		return fmt.Errorf("OS not supported")
+	}
+
+	cParams = append(cParams, fname)
+	cPath, err := exec.LookPath(cName)
+
+	if err != nil {
+		return err
+	}
+	err = exec.Command(cPath, cParams...).Run()
+	time.Sleep(2 * time.Second)
+
+	return err
+}
